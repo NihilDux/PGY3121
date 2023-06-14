262dd20de57efc14c7043cc95db2268a747dccf3
NihilDux <ang.curin@duocuc.cl>
2023-06-14 15:05:28 -0400

    Static Files

--- grupocero/settings.py
+++ grupocero/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -104,6 +104,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_URL = '/signin'
 # Default primary key field type
