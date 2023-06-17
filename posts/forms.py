d05e53751d40989ed9d9cdd320679d59c72f1d43
NihilDux <ang.curin@duocuc.cl>
2023-06-17 10:00:05 -0400

    Imagen centrada

--- posts/forms.py
+++ posts/forms.py
@@ -5,8 +5,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['titulo','imagen', 'descripcion', 'relevante']
-        #widgets = {
-        #    'titulo': forms.TextInput(attrs={'class': 'form-control'}),
-        #    'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
-        #    'relevante': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
-        #}
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'relevante': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'}),
+        }
